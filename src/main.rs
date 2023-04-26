05fcdfcb1de9da72330ff8cae5d2cfb3a95ad2be
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-04-25 19:37:48 -0700

    Support indication when taking time to retrieve executables

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::collections::BTreeSet; // to keep the order
-use std::env;
-use std::fs;
 use std::os::unix::fs::PermissionsExt;
 use std::process::exit;
+use std::{env, fs};
 
 use colored::*;
+use indicatif::{ProgressBar, ProgressStyle};
 use suggest::Suggest;
 
-fn find_executables() -> BTreeSet<String> {
+fn find_executables(pb: ProgressBar) -> BTreeSet<String> {
     let paths = env::var_os("PATH").unwrap();
     let mut executables = BTreeSet::<String>::new();
 
     for path in env::split_paths(&paths) {
+        pb.inc(1);
+
         if let Ok(entries) = fs::read_dir(path) {
             for entry in entries.flatten() {
                 let path = entry.path();
@@ -28,8 +30,22 @@ fn find_executables() -> BTreeSet<String> {
             }
         }
     }
+    pb.finish_and_clear();
 
     executables
+}
+
+fn get_executables() -> BTreeSet<String> {
+    let spinner_style =
+        ProgressStyle::with_template("{prefix} {spinner} Finding similar executables")
+            .unwrap()
+            .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
+
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(spinner_style);
+    pb.set_prefix(format!("{:>6}", "==>".green()));
+
+    find_executables(pb)
 }
 
 const NOTFOUND: i32 = 127;
@@ -45,8 +61,9 @@ fn main() {
 
     let cmd_name = &args[1];
     let cmd_args = &args[2..].join(" ");
-    let executables = find_executables();
     println!("{err} command not found: {} {cmd_args}", cmd_name.red());
+
+    let executables = get_executables();
     if let Some(sugg) = executables.suggest_with_dist(cmd_name, Some(2)) {
         println!("{:>6} Did you mean \"{}\"?", "==>".green(), sugg.green());
     }
