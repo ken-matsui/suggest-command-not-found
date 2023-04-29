b4d36470e76c3a78b3f0fc9c4c32a36c267da244
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-04-28 20:30:10 -0700

    Some tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::BTreeSet; // to keep the order
+use std::env;
+use std::fs::read_dir;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 use std::process::exit;
-use std::{env, fs};
 
 use colored::*;
 use suggest::Suggest;
@@ -10,17 +11,17 @@ use suggest::Suggest;
 const PATH: &str = "PATH";
 
 #[inline]
-fn is_executable(metadata: &fs::Metadata) -> bool {
-    metadata.permissions().mode() & 0o111 != 0
+fn is_executable(path: &Path) -> bool {
+    if let Ok(metadata) = path.metadata() {
+        metadata.permissions().mode() & 0o111 != 0
+    } else {
+        false
+    }
 }
 
 #[inline]
 fn is_executable_file(path: &Path) -> bool {
-    if let Ok(metadata) = path.metadata() {
-        path.is_file() && is_executable(&metadata)
-    } else {
-        false
-    }
+    path.is_file() && is_executable(path)
 }
 
 fn find_executables() -> BTreeSet<String> {
@@ -28,7 +29,7 @@ fn find_executables() -> BTreeSet<String> {
 
     if let Some(paths) = env::var_os(PATH) {
         for path in env::split_paths(&paths) {
-            if let Ok(entries) = fs::read_dir(path) {
+            if let Ok(entries) = read_dir(path) {
                 for entry in entries.flatten() {
                     let path = entry.path();
                     if is_executable_file(&path) {
@@ -49,18 +50,17 @@ fn find_executables() -> BTreeSet<String> {
 const NOTFOUND: i32 = 127;
 
 fn main() {
-    let err = "Error:".red();
-    let not_found_msg = format!("{err} command not found:");
+    let err_msg = format!("{} command not found:", "Error:".red());
 
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        eprintln!("{not_found_msg} \"\"");
+        eprintln!("{err_msg} \"\"");
         exit(NOTFOUND);
     }
 
     let cmd_name = &args[1];
     let cmd_args = &args[2..].join(" ");
-    eprintln!("{not_found_msg} {} {cmd_args}", cmd_name.red());
+    eprintln!("{err_msg} {} {cmd_args}", cmd_name.red());
 
     let executables = find_executables();
     if let Some(sugg) = executables.suggest_by(cmd_name, 2) {
